6be519661f7a17c0142c13aa48d2400068589948
Richard Guy <rich.guy@cantab.net>
2023-04-06 19:35:40 +0100

    EmailSummary object

--- summarise_emails.py
+++ summarise_emails.py
@@ -6,38 +6,43 @@ from bs4 import BeautifulSoup
 from mailparser_reply import EmailReplyParser
 import openai
 
-def summarise_email(email_str: str) -> str:
-    """Extract the latest reply from an email thread and return a short summary
-    """
-    # parse plain text body from raw email
-    msg = message_from_string(email_str)
-    if msg.is_multipart():
-        for part in msg.walk():
-            if 'text/plain' in part.get_content_type():
-               body = part.get_payload(decode=True)
-    else:
-        body = msg.get_payload(decode=True)
+class EmailSummary():
+    def __init__(self, email_str):
+        # parse email message from string
+        self.msg = message_from_string(email_str)
+        for k, v in self.msg.items():
+            setattr(self, k.lower(), v)
+        self._summarise_latest_reply()
 
-    # remove any formatting tags
-    soup = BeautifulSoup(body, features="html.parser")
-    mail_body = soup.get_text()
+    def _summarise_latest_reply(self):
+        # extract plain text body
+        if self.msg.is_multipart():
+            for part in self.msg.walk():
+                if 'text/plain' in part.get_content_type():
+                    body = part.get_payload(decode=True)
+        else:
+            body = self.msg.get_payload(decode=True)
+
+        # remove any formatting tags
+        soup = BeautifulSoup(body, features="html.parser")
+        mail_body = soup.get_text()
+        
+        # extract latest reply
+        rp = EmailReplyParser()
+        m = rp.read(mail_body)
+        latest_reply = m.replies[0].body
+
+        # summarise latest reply using OpenAI
+        prompt = f"Summarise this email in one sentence: {latest_reply}"
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        self.summary = response['choices'][0]['message']['content']
     
-    # extract latest reply
-    rp = EmailReplyParser()
-    m = rp.read(mail_body)
-    latest_reply = m.replies[0].body
-
-    # summarise latest reply using OpenAI
-    prompt = f"Summarise this email in one sentence: {latest_reply}"
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return(response['choices'][0]['message']['content'])
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
@@ -45,5 +50,4 @@ if __name__ == '__main__':
     
     with open(args.filename, "r") as f:
         raw_email = f.read()
-    text = summarise_email(raw_email)
-    print(text)
+    print(EmailSummary(raw_email).summary)
---
+++ summarise_folder.py
@@ -0,0 +1,36 @@
+from summarise_emails import EmailSummary
+import json
+import argparse
+from pathlib import Path
+from tqdm import tqdm
+
+def summarise_folder(path: str) -> list:
+    files = Path(path).absolute().iterdir()
+    results = []
+    for item in tqdm(files):
+        if item.is_file():
+            with open(item, "r") as f:
+                raw_email = f.read()
+            msg = EmailSummary(raw_email)
+            results.append(
+                {
+                    "subject": msg.subject,
+                    "from": getattr(msg, "from"),
+                    "to": msg.to,
+                    "date": msg.date,
+                    "uri": item.as_uri(),
+                    "summary": msg.summary,
+                }
+            )
+    return results 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder')
+    args = parser.parse_args()
+    
+    summary = summarise_folder(args.folder)
+    json_summary = json.dumps(summary, indent=4)
+
+    with open(f"summary.json", "w") as f:
+        f.write(json_summary)
